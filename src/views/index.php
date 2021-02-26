b4cedb0517965119d8f111854687b50959fa0fc6
kobayashidaisuke <daiyusket1226@gmail.com>
2021-02-27 04:17:32 +0900

    微調整

--- src/views/index.php
+++ src/views/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name=”description” content=”どこでも誰とでもコミュニケーションが取れる掲示板です。利用方法は掲示板に表示されています。自身の発言には責任を持ちましょう。”>
     <link rel="stylesheet" href="../stylesheets/css/app.css">
-    <link rel="icon" href=".https://raw.githubusercontent.com/kobayashidaisuke/easyboards/bce3d290cd5643d54c0a6dafb47d5c634f2999c7/src/gallery/easyboard.svg">
+    <link rel="icon" href="https://user-images.githubusercontent.com/62587652/109342535-5aa05900-78af-11eb-8f7e-1a37e6985d1c.png">
     <title>コミュコミュ！&nbsp;~コロナ禍で失われたコミュニケーションを取り戻そう~</title>
 </head>
 
